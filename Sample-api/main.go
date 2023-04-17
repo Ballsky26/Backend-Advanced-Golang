9eb16cba9da6948b9c81d5e4109fdb54b1e4dbae
Ballsky22 <ballskyteam22@gmail.com>
2023-04-17 23:44:35 +0700

    Buat Simple API with Gin Framework

--- Sample-api/main.go
+++ Sample-api/main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Test struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
@@ -21,6 +26,33 @@ func setupRouter() *gin.Engine {
 		ctx.JSON(http.StatusOK, gin.H{
 			"status": "success",
 			"value":  "Hello world",
+		})
+	})
+
+	v1 := r.Group("v1")
+	v1.GET("/user/:name", func(ctx *gin.Context) {
+		param := ctx.Param("name")
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "success",
+			"value":  param,
+		})
+	})
+
+	v1.POST("user", func(ctx *gin.Context) {
+		var data Test
+		ctx.BindJSON(&data)
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "success",
+			"value":  data,
+		})
+	})
+
+	v1.GET("/user", func(ctx *gin.Context) {
+		query := ctx.Query("name")
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "success",
+			"value":  query,
 		})
 	})
 	return r
