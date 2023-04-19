07a3f9c412ca62c91ae72783daff656e681898fb
Ballsky22 <ballskyteam22@gmail.com>
2023-04-20 00:03:09 +0700

    Make Read

--- Simple-Api-Framework/main.go
+++ Simple-Api-Framework/main.go
@@ -5,6 +5,7 @@ import (
 	_ "database/sql" // add this
 	"log"
 	"net/http"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq" // add this
@@ -16,6 +17,29 @@ type newStudent struct {
 	Student_age      uint64 `json:"student_age" binding:"required"`
 	Student_address  string `json:"student_address" binding:"required"`
 	Student_phone_no string `json:"student_phone_no" binding:"required"`
+}
+
+func rowToStruct(rows *sql.Rows, dest interface{}) error {
+	destv := reflect.ValueOf(dest).Elem()
+
+	args := make([]interface{}, destv.Type().Elem().NumField())
+
+	for rows.Next() {
+		rowp := reflect.New(destv.Type().Elem())
+		rowv := rowp.Elem()
+
+		for i := 0; i < rowv.NumField(); i++ {
+			args[i] = rowv.Field(i).Addr().Interface()
+		}
+
+		if err := rows.Scan(args...); err != nil {
+			return err
+		}
+
+		destv.Set(reflect.Append(destv, rowv))
+	}
+
+	return nil
 }
 
 func postHandler(c *gin.Context, db *sql.DB) {
@@ -33,6 +57,45 @@ func postHandler(c *gin.Context, db *sql.DB) {
 	c.JSON(http.StatusBadRequest, gin.H{"message": "error"})
 }
 
+func getAllHandler(c *gin.Context, db *sql.DB) {
+	var newStudent []newStudent
+
+	row, err := db.Query("select * from students")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	rowToStruct(row, &newStudent)
+
+	if newStudent == nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "data not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": newStudent})
+
+}
+
+func getHandler(c *gin.Context, db *sql.DB) {
+	var newStudent []newStudent
+
+	studentId := c.Param("student_id")
+
+	row, err := db.Query("select * from students where student_id = $1", studentId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	rowToStruct(row, &newStudent)
+
+	if newStudent == nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "data not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": newStudent})
+}
+
 func setupRouter() *gin.Engine {
 	conn := "postgresql://postgres:1234@127.0.0.1/postgres?sslmode=disable"
 	db, err := sql.Open("postgres", conn)
@@ -46,6 +109,13 @@ func setupRouter() *gin.Engine {
 		postHandler(ctx, db)
 	})
 
+	r.GET("/student", func(ctx *gin.Context) {
+		getAllHandler(ctx, db)
+	})
+
+	r.GET("/student/:student_id", func(ctx *gin.Context) {
+		getHandler(ctx, db)
+	})
 	return r
 }
 
